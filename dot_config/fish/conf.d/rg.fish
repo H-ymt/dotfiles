9447599dc4786416929a61942f3dd3a600a33460
H-ymt <y.handai1272@gmail.com>
2026-05-04 23:45:08 +0900

    feat(fish): ripgrep 用の abbr を追加

---
+++ dot_config/fish/conf.d/rg.fish
@@ -0,0 +1,8 @@
+# ripgrep abbreviations
+abbr --add rgi 'rg -i'
+abbr --add rgl 'rg -l'
+abbr --add rgc 'rg -C 3'
+abbr --add rgh 'rg --hidden'
+abbr --add rgw 'rg -w'
+abbr --add rgt 'rg -t'
+abbr --add rgf 'rg -F'
